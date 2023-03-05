0f0115b41a35a7ddea628e46187bda64c8370cb4
Najaf Sharifov <nwerifov@mail.ru>
2023-03-06 00:48:34 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+//Task 2.10
+//1
+/*let num = 1;
+
+for (let i = 1; i < 10; i++) {
+  num *= i;
+  console.log(num);
+}*/
+
+//2
+/*let text = prompt("Enter text");
+for(let i = text.length - 1;i>=0;i--){
+    console.log(text[i]);
+}*/
+
+//3
+
+/*let str = prompt("Enter text");
+for(let i = 0;i<str.length;i++){
+   if(str[i] == "."){
+     console.log(i);
+   }
+}*/
+
+//4
+/*
+let str = prompt("Enter text");
+for (let i = 0; i < str.length; i++) {
+  if (str[i].match(/[a-z]/) || str[i].match(/[A-Z]/)) {
+    console.log("Sətrdə rəqəmlər yoxdur");
+  } else {
+    console.log("Sətrdə rəqəmlər var");
+  }
+}*/
+
+
